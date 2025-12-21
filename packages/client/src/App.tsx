e7cb837a8ddced6d79c7f5578ae937be073c8c8b
Soumya Ranjan <soumya.sahoo@uneecops.in>
2025-12-21 21:44:20 +0530

    connect frontend and backend

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api/hello').then(res => res.json()).then(data => setMessage(data.message))
+  }, [])
+
+  return <p>{message}</p>
+}
+
+export default App
---
+++ packages/server/index.ts
@@ -0,0 +1,19 @@
+import express, { type Request, type Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({message: 'Hello World!'})
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    
+})
