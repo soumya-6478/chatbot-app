d20d9436899f9a546f70f1b43804dab6cc3629b6
Soumya Ranjan <soumya.sahoo@uneecops.in>
2025-12-22 12:23:23 +0530

    format code base with prettier with command line

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react"
-import { Button } from "./components/ui/button"
-
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('')
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello').then(res => res.json()).then(data => setMessage(data.message))
-  }, [])
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className="p-4">
-    <p className="font-bold text-3xl">{message}</p>
-    <Button>Click Me</Button>
-    </div>
-  )
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,18 @@
-import express, { type Request, type Response } from 'express'
-import dotenv from 'dotenv'
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+   res.send('Hello World!');
+});
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello World!'})
-})
+   res.json({ message: 'Hello World!' });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    
-})
+   console.log(`Server is running on http://localhost:${port}`);
+});
